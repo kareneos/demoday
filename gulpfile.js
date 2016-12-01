5d58ace0bd22ad4a7fcbac07d2d48e31917eb625
danamorah <damoraherrera@gmail.com>
2016-12-01 12:04:57 -0300

    fix gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+var stylus = require('gulp-stylus');
 
 gulp.task ('default', ['lab', 'lab2']);
 
@@ -9,3 +10,14 @@ gulp.task ('lab', function(){
 gulp.task('lab2', function(){
     console.log("Hola mundo 2");
 });
+
+// tarea de carpeta
+gulp.task('one', function () {
+    return gulp.src('./src/style.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('./src/*.styl', ['one']);
+})
